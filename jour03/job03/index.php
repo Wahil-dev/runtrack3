e289b5c2c591a4a8b0d880acaba725b9e02d153e
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-04-03 16:09:50 +0200

    deplacer les images

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -13,15 +13,17 @@
         <span class="error-game">partie terminer !</span>
 
         <div class="container-imgs flex-r">
-            <img class="flex-r" src="img/logo1.pn" id="logo1" alt="1">
-            <img class="flex-r" src="img/logo4.pn" id="logo2" alt="2">
-            <img class="flex-r" src="img/logo7.pn" id="logo3" alt="3">
-            <img class="flex-r" src="img/logo2.pn" id="logo4" alt="4">
-            <img class="flex-r" src="img/logo5.pn" id="logo5" alt="5">
-            <img class="flex-r" src="img/logo8.pn" id="logo6" alt="6">
-            <img class="flex-r" src="img/logo3.pn" id="logo7" alt="7">
-            <img class="flex-r" src="img/logo9.pn" id="logo8" alt="8">
-            <img class="flex-r" src="img/logo3.pn" id="logo9" alt="9">
+            <div class="box-img">
+            </div>
+            <div class="box-img">
+                <img class="flex-r" src="img/imageonline/morceaux-2.png" id="morceaux-2" alt="2">
+            </div>
+            <div class="box-img">
+                <img class="flex-r" src="img/imageonline/morceaux-3.png" id="morceaux-3" alt="3">
+            </div>
+            <div class="box-img">
+                <img class="flex-r" src="img/imageonline/morceaux-4.png" id="morceaux-4" alt="4">
+            </div>
         </div>
     </div>
 
--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -1,29 +1,66 @@
-const containerImages = document.querySelector(".container-imgs");
+let containerImages = document.querySelector(".container-imgs");
 const recommencerBtn = document.querySelector("#recommencer");
 
-const images = ["logo1", "logo2", "logo3", "logo4", "logo5", "logo6", "logo7", "logo8", "logo9"];
+//const images = ["logo1", "logo2", "logo3", "logo4", "logo5", "logo6", "logo7", "logo8", "logo9"];
+const images = [
+    ["", "morceaux-2"], 
+    ["morceaux-3", "morceaux-4"]
+];
 
 let firstClick = 0;
+let imageRemoved = "morceaux-1";
+let imageIsCreated = false;
+let indexOfImageClicked = []
 
-for(let i = 0; i < images.length; i++) {
-    let image = document.querySelector(`#${images[i]}`);
-    image.addEventListener("click", (e) => {
-        if(isFirstClick()) {
-            image.remove();
-        } else {
-            moveImage(e)
+document.addEventListener("click", () => {
+    for(let i = 0; i < images.length; i++) {
+        for(let j = 0; j < images[i].length; j++) {
+            if(images[i][j] != "") {
+                let image = document.querySelector(`#${images[i][j]}`);
+                image.addEventListener("click", (e) => {
+                    images[i][j] = "";  
+                    indexOfImageClicked = [i, j]
+                    moveImage(e, indexOfImageClicked)
+
+                    //Upload images 
+                    uploadImage();
+                })
+            }
         }
-    })
-}
-
-function moveImage(e) {
-    console.log(e.target);
-}
-
-function isFirstClick(e) {
-    if(firstClick < 1) {
-        firstClick = 1;
-        return true;
     }
-    return false;
+})
+
+function moveImage(e, indexOfImageClicked) {
+    let imageClicked = e.target.id;
+    for(let i = 0; i < images.length; i++) {
+        for(let j = 0; j < images[i].length; j++) {    
+            if(images[i][j] == "" && (i != indexOfImageClicked[0] || j != indexOfImageClicked[1])) {
+                images[i][j] = imageClicked;
+            }
+        }
+    }
+}
+
+
+function uploadImage() {
+    console.log(images)
+    containerImages.innerHTML = "";
+    for(let i = 0; i < images.length; i++) {
+            for(let j = 0; j < images[i].length; j++) {
+                if(images[i][j] != "") {
+                    let image = `
+                        <div class="flex-r box-img">
+                            <img class="flex-r" src="img/imageonline/${images[i][j]}.png" id="${images[i][j]}" alt="${images[i][j]}">
+                        </div>
+                    `;
+                    containerImages.innerHTML += image;
+                } else {
+                    let image = `
+                    <div class="flex-r box-img">
+                    </div>
+                `;
+                containerImages.innerHTML += image;
+                }
+            }
+    }
 }
